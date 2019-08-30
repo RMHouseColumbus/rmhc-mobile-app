c07859d9ddd83b56744e7ce49effda0f0d6e0107
dnMarc <d.marcus.at.work@gmail.com>
2019-08-30 18:11:50 -0400

    Transportation now pulls content from S3

--- src/screens/transportation/transportation.tsx
+++ src/screens/transportation/transportation.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, TextStyle, View, ViewStyle} from "react-native"
+import {Text, TextStyle, View, ViewStyle, StatusBar, ActivityIndicator, StyleSheet} from "react-native"
 import {spacing} from "../../components/shared/spacing";
 import {Header} from "../../components/header/header";
 import {ContentService} from "../../services/ContentService";
@@ -9,135 +9,123 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 import LeftArrow from '../../images/left_arrow.svg';
 import { getStatusBar } from '../../components/shared/statusBar';
 import { DeliveryScreenProps } from '../delivery/delivery';
+import { NavigationScreenProps } from 'react-navigation';
 
 const FULL: ViewStyle = {
     flex: 1,
     padding: 20
 };
 
-export interface TransportationState {
-    content: any
+
+interface TransportationProps extends NavigationScreenProps {}
+
+interface TransportationState {
+    isLoading: boolean,
+    rideShares: any
+    cabCompanies: any
+    publicTrans: any
 }
 
-const CONTENT_RAW = {transportation: {
-    Rideshare: [
-      {
-        id: 0,
-        name: "Lyft",
-        url: "https://www.lyft.com"
-      },
-      {
-        id: 1,
-        name: "Uber",
-        url: "https://www.uber.com"
-      }
-    ],
-    CabServices: [
-      {
-        id: 0,
-        name: "Yellow Cab of Columbus",
-        contact: "(614) 444-4445"
-      },
-      {
-        id: 1,
-        name: "Express Cab of Columbus",
-        contact: "(614) 822-8666"
-      },
-      {
-        id: 2,
-        name: "Ohio Taxi Service",
-        contact: "(614) 562-7959"
-      }
-    ],
-    PublicTransportation: [
-      {
-        id: 0,
-        name: "Central Ohio Transit Authority (COTA)",
-        sub: "COTA Trip Planner:",
-        contact: "https://www.cota.com/trip-planner"
-      }
-    ]
-  }
-}
+export default class Transportation extends React.Component<TransportationProps, TransportationState> {
 
-export default class Transportation extends React.Component<DeliveryScreenProps, TransportationState> {
+    static navigationOptions = {
+        title: 'Transportation',
+    };
 
     public constructor(props) {
         super(props);
         this.state = {
-            content : CONTENT_RAW
+            isLoading: true,
+            rideShares: [],
+            cabCompanies: [],
+            publicTrans: []
         }
     }
 
-    // componentDidMount(): void {
-    //     const data = ContentService.contentForPage("transportation");
-    //     this.setState({
-    //         content: data
-    //     })
-    // }
-
-    static navigationOptions = {
-        title: 'TRANSPORTATION',
-    };
-
-    render() {
-        const CONTENT = this.state.content;
-        return (
-            <View style={FULL}>
-                {
-                    getStatusBar()
+    componentDidMount(): void {
+        ContentService.contentForPage("transportation")
+            .then((result) => {
+                    this.setState({
+                        isLoading: false,
+                        rideShares: result.content.Rideshare,
+                        cabCompanies: result.content.CabServices,
+                        publicTrans: result.content.PublicTransportation    
+                    })
                 }
-                <View>
-                    <TouchableOpacity style={{height: 50, flexDirection: 'row'}} onPress={() => this.props.navigation.navigate("Neighborhood")}>
-                        <LeftArrow stle={{flex: 1}} width={20} height={20}></LeftArrow>
-                        <Text style={{flex: 1, marginLeft: 5}}>Back</Text>
-                    </TouchableOpacity>
-                    <View style={SECTION}>
-                        <Text style={CONTENTSTYLE}>Rideshare</Text>    
-                        {
-                            CONTENT.transportation.Rideshare.map(c => {
-                                return (
-                                    this.rideShareInfo(c)
-                                )
-                            })
-                        }
-                    </View>
-                    
-                    <View style={SECTION}>
-                        <Text style={CONTENTSTYLE}>Cab Service</Text>
-                        {
-                            CONTENT.transportation.CabServices.map(c => {
-                                return (
-                                    this.cabServiceData(c)
-                                )
-                            })
-                        }
-                    </View>
-
-                    <View style={SECTION}>
-                        <Text style={CONTENTSTYLE}>Public Transportation</Text>
-                        {
-                            CONTENT.transportation.PublicTransportation.map(c => {
-                                return (
-                                    this.publicTransportationData(c)
-                                )    
-                            })
-                        }
-                    </View>
-
-                </View>
-            </View>
-        );
+            )
     }
 
-    // private carrierRow(c: any) {
-    //     return (
-    //         <View key={c.id}>
-    //             <Text style={CONTENTSTYLE}>{c.name}</Text>
-    //             <Text style={LINKSTYLE}>{c.url}</Text>
-    //         </View>
-    //     )
-    // }
+    render() {
+
+        const isLoading = this.state.isLoading;
+        const rideShares = this.state.rideShares;
+        const cabCompanies = this.state.cabCompanies;
+        const publicTrans = this.state.publicTrans;
+
+        if (isLoading){
+            return this.loadingComponent();
+        } else {
+            return (
+                <View style={FULL}>
+                    {
+                        getStatusBar()
+                    }
+                    <View>
+                        <TouchableOpacity style={{height: 50, flexDirection: 'row'}} onPress={() => this.props.navigation.navigate("Neighborhood")}>
+                            <LeftArrow stle={{flex: 1}} width={20} height={20}></LeftArrow>
+                            <Text style={{flex: 1, marginLeft: 5}}>Back</Text>
+                        </TouchableOpacity>
+                        <View style={SECTION}>
+                            <Text style={CONTENTSTYLE}>Rideshare</Text>    
+                            {
+                                rideShares.map(c => {
+                                    return (
+                                        this.rideShareInfo(c)
+                                    )
+                                })
+                            }
+                        </View>
+                        
+                        <View style={SECTION}>
+                            <Text style={CONTENTSTYLE}>Cab Service</Text>
+                            {
+                                cabCompanies.map(c => {
+                                    return (
+                                        this.cabServiceData(c)
+                                    )
+                                })
+                            }
+                        </View>
+    
+                        <View style={SECTION}>
+                            <Text style={CONTENTSTYLE}>Public Transportation</Text>
+                            {
+                                publicTrans.map(c => {
+                                    return (
+                                        this.publicTransportationData(c)
+                                    )    
+                                })
+                            }
+                        </View>
+    
+                    </View>
+                </View>
+            );
+        } 
+    }
+
+    private loadingComponent() {
+        return (
+            <View style={{flex: 1}}>
+                <StatusBar barStyle="dark-content" backgroundColor="#FFFFFF"/>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                    <View style={main.container}>
+                        <ActivityIndicator/>
+                    </View>
+                </View>
+            </View>
+        )
+    }
 
     private rideShareInfo(c: any) {
         return (
@@ -170,9 +158,14 @@ export default class Transportation extends React.Component<DeliveryScreenProps,
 
 const BOLD: TextStyle = {fontWeight: "bold"};
 
-
-
 const SECTION: ViewStyle = {
     marginTop: spacing[5]
 };
 
+const main = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginLeft: 20,
+        top: 50
+    }
+});
