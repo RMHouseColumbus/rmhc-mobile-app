ad2145c0ecb890718792279f36291526ad925c6d
Ahmed <eng.amostafa@gmail.com>
2019-08-30 14:25:02 -0400

    Styling home page

--- src/components/base/home.js
+++ src/components/base/home.js
@@ -2,24 +2,34 @@ import React from 'react';
 import { StyleSheet, Text, View, StatusBar, TouchableOpacity } from 'react-native';
 import { Container } from 'native-base'
 import BaseFooter from './footer.js'
+import Logo from '../../images/logo_landscape.svg'
+import Henry from '../../images/henry_landingpage.svg'
+import Bubble from '../../images/bubble.svg'
 
 export default class Home extends React.Component {
 
     static navigationOptions = {
-        title: 'Welcome',
+        title: 'RMHC Central Ohio',
     }
     render() {
         return (
             <View style={styles.main}>
+                <Henry navigation={this.props.navigation} style={{ position: 'absolute', bottom: 249, zIndex: 1000, left: 50}}/>
                 <StatusBar backgroundColor="#4872ae" barStyle="dark-content" style={{color:"#FFFFFF"}}/>
-                <View style={{ flex: 0.9 }}>
-                    <View style={styles.container}>
-                        <Text style={styles.welcomeText}>Welcome</Text>
-                        <Text style={{ fontSize: 30, padding: 5 }}>to</Text>
-                        <Text style={{ fontSize: 40 }}>Central Ohio</Text>
-                    </View>
+
+                <View style={{ flex: 1 }}>
+                    <Text style={styles.welcomeText}>Welcome To</Text>
                 </View>
-                <View style={{ flex: 0.1 }}>
+
+                <Logo style={{flex: 5, alignSelf: 'center'}}></Logo>
+
+                <View style={{ flex: 3, alignSelf: 'center', flexDirection: 'row'}}>
+                    <Bubble navigation={this.props.navigation} style={{ position: 'absolute', top: 20, zIndex: 1000, left: -15}}/>
+                </View>
+                <View style={{ flex: 1.5, backgroundColor: "#4872ae", padding: 40}}>
+                    <Text style={{color: '#ffffff', fontSize: 40, alignSelf: 'center'}}>Manage Your Stay With Us.</Text>
+                </View>
+                <View style={{ flex: 1 }}>
                     <BaseFooter navigation={this.props.navigation} />
                 </View>
             </View>
@@ -29,41 +39,23 @@ export default class Home extends React.Component {
 
 const styles = StyleSheet.create({
     main:{
-        flex: 1, backgroundColor: "#F1F1F1" 
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        paddingTop: 20,
+        paddingBottom: 0
     },
     container: {
         flex: 1,
-        marginLeft: 20,
-        top: 50,
     },
     welcomeText: {
-        fontFamily: "System",
-        fontSize: 45,
-        color: 'red'
+        flex: 1,
+        alignSelf: 'center',
+        fontSize: 33,
+        fontWeight: "600",
+        fontStyle: "normal",
+        letterSpacing: 0,
+        textAlign: "center",
+        color: '#da291c'
     }
-
 });
-
-
-// const styles = StyleSheet.create({
-//    
-
-//     },
-//     whiteContainer: {
-//       backgroundColor: '#FFFFFF'
-
-//     },
-//     yellowStatusBar:{
-//       backgroundColor:"#FCCB00"
-//     },
-//     whiteStatusBar:{
-//       backgroundColor:"#FFFFFF",
-
-//     },
-
-//     headerText:{
-//       fontFamily:"System",
-//       fontSize:35,
-//       color:'#000000'
-//     }
-//   });
