88d24cf73a08900f2db6fbd72afa162b81339a02
mgersty <mgerstem@gmail.com>
2019-08-31 13:58:20 -0400

    Facilities:  Updating styles for iOS

--- src/components/facilities/home.js
+++ src/components/facilities/home.js
@@ -14,7 +14,7 @@ export default class Facilities extends React.Component {
             <View style={styles.main}>
                 <StatusBar backgroundColor="#4872ae" barStyle="light-content" />
                 <View style={{ flex: 0.9 }}>
-                    <ImageBackground source={require('./floorplanbackground.jpg')} style={{ width: '100%', height: '100%' }} />
+                    <ImageBackground source={require('../../../images/floorplan_bckgrd.png')} style={{ width: '100%', height: '100%' }} />
                     <View style={styles.floorPlanBackground} />
                     <View style={styles.floorPlanTextContainer}>
                         <Text style={styles.floorPlanText}>FLOOR PLAN</Text>
@@ -45,11 +45,11 @@ const styles = StyleSheet.create({
         fontSize: 45,
         color: 'black'
     },
-    floorPlanBackground: { borderColor: 'black', borderWidth: 5, width: '60%', height: '40%', backgroundColor: '#FFFFFF', position: 'absolute', top: '35%', left: '17%', opacity: 0.85 },
-    floorPlanTextContainer: { justifyContent: 'center', alignItems: 'center', width: 120, position: 'absolute', top: '39%', left: '33%' },
+    floorPlanBackground: { borderColor: 'black', borderWidth: 5, width: '60%', height: '35%', backgroundColor: '#FFFFFF', position: 'absolute', top: '35%', left: '20%', opacity: 0.85 },
+    floorPlanTextContainer: { justifyContent: 'center', alignItems: 'center', width: 120, position: 'absolute', top: '39%', left: '34%' },
     floorPlanText: { fontSize: 35, fontWeight: "bold", textAlign:'center',justifyContent:'center',},
     floorPlanButton: {
-        top:'20%',
+        top:'25%',
         textAlign:'center',
         justifyContent:'center',
         width:120,
