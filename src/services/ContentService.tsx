6c49f6e2ea0d6cb29fce7bd017c2a9691dbb8159
dnMarc <d.marcus.at.work@gmail.com>
2019-08-31 10:50:54 -0400

    change content service to not use cache

--- src/services/ContentService.tsx
+++ src/services/ContentService.tsx
@@ -2,9 +2,20 @@ export class ContentService {
 
     private static S3_URL = "https://rmhc-central-oh.s3.us-east-2.amazonaws.com/content.json";
 
-
+    
+    
     public static load() {
-        return fetch(this.S3_URL)
+
+        const myHeaders = new Headers();
+        myHeaders.append('pragma', 'no-cache');
+        myHeaders.append('cache-control', 'no-cache');
+
+        const myInit = {
+            method: 'GET',
+            headers: myHeaders
+          };
+
+        return fetch(this.S3_URL, myInit)
             .then(res => {
                 return res.json()
             });
