afb0557e6f1382eee7194975e1a5d14753be271a
Matthew Gerstemeier <mgerstem@gmail.com>
2019-09-05 21:02:07 -0400

    Adding a quick simple link to open location in google and apple maps

--- src/views/main/screens/findus.tsx
+++ src/views/main/screens/findus.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar, ImageBackground, Linking } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, ImageBackground, Linking, Platform } from 'react-native';
 import { NavigationScreenProps } from 'react-navigation';
 import BaseFooter from '../../shared/footer'
 import {HEADERSTYLEBLUE, HEADERTITLESTYLEWHITE} from '../../shared/fonts';
 
 export interface FindUscreenProps extends NavigationScreenProps {
-}
-
-export default class FindUs extends React.Component <FindUscreenProps,{}> {
+}Platform.select({ ios: 'maps:0,0?q=', android: 'geo:0,0?q=' });export default class FindUs extends React.Component <FindUscreenProps,{}> {
 
     static navigationOptions = {
         title: 'Find Us',
@@ -15,21 +13,27 @@ export default class FindUs extends React.Component <FindUscreenProps,{}> {
         headerTitleStyle: HEADERTITLESTYLEWHITE
     }
     render() {
+        const lat = "39.951073"
+        const long = "-82.978959" 
+        const rmhcPhoneNumber = "tel:614-227-3700"
+        const rmhcEmail = "mailto:rmhccommunications@rmhc-centralohio.org"
+        const rmhcGeoCoordinates = Platform.select({ ios: 'maps:'+lat+','+long, android: 'geo:'+lat+','+long });
         return (
             <View style={styles.main}>
                 
                 <StatusBar backgroundColor="#4872ae" barStyle="dark-content" style={{color:"#FFFFFF"}}/>
 
-                <View style={{ flex: 0.3, marginLeft:'2%', borderBottomWidth:1}}>
-                    <View style={{flex:1, top:'5%', width:'80%'}}>
+                <View style={{ flex: 0.4, marginLeft:'2%'}}>
+                    <View style={styles.container}>
                         <Text style={styles.locationTextMain}>Ronald McDonald House {"\n"}Charities Central OH</Text>
                         <Text style={styles.locationTextSub}>711 E Livingston Ave.{"\n"}Columbus, OH 43205</Text>
-                        <Text style={styles.locationLinkStyle} onPress={() => Linking.openURL("tel:614-227-3700")}>614-227-3700</Text>
-                        <Text style={styles.locationLinkStyle} onPress={() => Linking.openURL("mailto:rmhccommunications@rmhc-centralohio.org")}>rmhccommunications@rmhc-centralohio.org</Text>
+                        <Text style={styles.locationLinkStyle} onPress={() => Linking.openURL(rmhcPhoneNumber)}>614-227-3700</Text>
+                        <Text style={styles.locationLinkStyle} onPress={() => Linking.openURL(rmhcEmail)}>rmhccommunications@rmhc-centralohio.org</Text>
+                        <Text style={styles.locationLinkStyle} onPress={() => Linking.openURL(rmhcGeoCoordinates)}>Go to Maps</Text>
                     </View>
                 
                 </View>
-                <View style={{flex:0.7, marginLeft:'1%',width:'98%'}}>
+                <View style={{flex:0.6, marginLeft:'1%',width:'98%'}}>
                         <ImageBackground source={require('./assets/findusmap.png')} imageStyle={{resizeMode: 'stretch', overflow:'visible'}} style={{width: '100%', height: '100%'}} />
                 </View>
                 <View style={{ flex: 0.1}}>
@@ -48,9 +52,7 @@ const styles = StyleSheet.create({
         
         paddingBottom: 0
     },
-    container: {
-        flex: 1,
-    },
+    container: {flex:1, top:'5%', width:'80%'},
     locationTextMain: {
         
         
