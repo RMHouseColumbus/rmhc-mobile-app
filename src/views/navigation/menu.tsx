d4d735ed5a21712934e270ecceaa273c7805a4cc
Ahmad Farag <ahmad.k.farag@gmail.com>
2020-01-28 17:48:53 -0500

    #68 relabel FAQ to your stay

--- src/views/navigation/menu.tsx
+++ src/views/navigation/menu.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import { StyleSheet, Text, TouchableOpacity, View, Linking, Image } from 'react-native'
+import {Linking, StyleSheet, Text, TouchableOpacity, View} from 'react-native'
 import Facebook from "./assets/facebook.svg";
 import Linkedin from "./assets/linkedin.svg";
 import Youtube from "./assets/youTube.svg";
 import Twitter from "./assets/twitter.svg";
-import { NavigationScreenProps } from "react-navigation";
-
+import {NavigationScreenProps} from "react-navigation";
 
 
 export default class Menu extends React.Component<NavigationScreenProps, {}> {
@@ -25,7 +24,7 @@ export default class Menu extends React.Component<NavigationScreenProps, {}> {
                     {this.navLink("Facilities", "Facilities/Floor Plan")}
                     {this.navLink("Neighborhood", "Neighborhood Guide")}
                     {this.navLink("About", "About")}
-                    {this.navLink("Faq", "FAQ")}
+                    {this.navLink("Faq", "Your Stay")}
                     <TouchableOpacity style={styles.SubmitButtonStyle} activeOpacity={.5}
                         onPress={() => Linking.openURL("http://rmhc-centralohio.org/app_donation_page/")}>
                         <Text style={styles.TextStyle}>Donate</Text>
