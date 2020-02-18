cd6238bf9e883a3e37554150e5d1d0982d16c6c2
Ahmad Farag <ahmad.k.farag@gmail.com>
2020-02-17 21:39:38 -0500

    Fixed #69. Added new svg with welcome in the heart. Added transparent box around the text to get the FAQ navigation while clicking henry still goes to youtube

--- src/views/main/screens/home.tsx
+++ src/views/main/screens/home.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import {Dimensions, Linking, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {Grid, Row} from 'native-base';
-import {NavigationNavigatorProps, NavigationScreenProps} from 'react-navigation';
+import {NavigationScreenProps} from 'react-navigation';
 import BaseFooter from '../../shared/footer';
 import Logo from '../../../images/logo_landscape.svg';
-import Henry from '../../../images/henry_landingpage.svg';
+import Henry from '../../../images/landing_new.svg';
 import {HEADERSTYLEBLUE} from '../../shared/fonts';
 import SplashScreen from 'react-native-splash-screen';
-import Youtube from '../../navigation/assets/youTube.svg';
 import {setStatusBar} from '../../shared/status-bar';
 
 
-export interface HomeScreenProps extends NavigationNavigatorProps {
+export interface HomeScreenProps extends NavigationScreenProps {
 }
 
-const YOUTUBE_LINK= "https://youtu.be/9ypZmfHSiXg";
+const YOUTUBE_LINK = "https://youtu.be/9ypZmfHSiXg";
 
-export default class Home extends React.Component<NavigationScreenProps, {}> {
+export default class Home extends React.Component<HomeScreenProps, {}> {
 
 
     static navigationOptions = {
@@ -43,56 +42,31 @@ export default class Home extends React.Component<NavigationScreenProps, {}> {
         return (
             <View style={styles.main}>
 
-                <Grid style={{ flex: 4 }}>
+                <Grid style={{flex: 4}}>
                     <Row>
                         <Text style={styles.welcomeText}>Welcome To</Text>
                     </Row>
-                    <Row style={{alignSelf: 'center' }}>
-                        <Logo style={{ flex: 1, alignSelf: 'center', position: 'relative', marginBottom: '1%' }} />
+                    <Row style={{alignSelf: 'center'}}>
+                        <Logo style={{flex: 1, alignSelf: 'center', position: 'relative', marginBottom: '1%'}}/>
                     </Row>
                 </Grid>
                 <View style={styles.henry}>
-                    <Henry width={'100%'} height={'100%'} />
-                    <TouchableOpacity
-                        style={{
-                            position: 'absolute',
-                            left: 220,
-                            bottom: '50%',
-                            alignItems: 'center'
-                        }}
-                        onPress={() => Linking.openURL(YOUTUBE_LINK)}>
-                        <Youtube {...SVG} />
-                        <Text style={{
-                            fontSize: 20,
-                            marginLeft: 8,
-                            fontFamily: 'Raleway-Regular',
-                            color: "#0078d7"
-                        }}>Welcome...</Text>
-                    </TouchableOpacity>
-                    <View
-                          style={{ flex: 1, backgroundColor: "4872ae", alignSelf: 'center', zIndex: 101, position: 'absolute', bottom: '20%' }}>
-                        <TouchableOpacity onPress={this.onClickOfYourState}>
-                            <Text style={styles.manage}>{"Manage Your\nStay with Us."}</Text>
+                    <Henry width={'100%'} height={'100%'} onPress={() => Linking.openURL(YOUTUBE_LINK)} />
+                        <TouchableOpacity style={styles.invisibleBox} onPress={this.onClickOfYourState}>
+                            <Text style={StyleSheet.absoluteFillObject} />
                         </TouchableOpacity>
-                    </View>
                 </View>
                 <View style={styles.footer}>
-                    <BaseFooter navigation={this.props.navigation} />
+                    <BaseFooter navigation={this.props.navigation}/>
                 </View>
             </View>
         );
     }
 }
-const SVG = {
-    // alignSelf: 'flex-end',
-    width: 20,
-    height: 20,
-    // marginBottom: spacing[4]
-};
 
 const styles = StyleSheet.create({
 
-    henry: { flex: 6, position: 'relative', top: '5%' },
+    henry: {flex: 6, position: 'relative', top: '5%', zIndex: -100},
     main: {
         flex: 1,
         flexDirection: 'column',
@@ -102,8 +76,6 @@ const styles = StyleSheet.create({
     },
     container: {
         flex: 1,
-
-
     },
     welcomeText: {
         flex: 1,
@@ -116,22 +88,21 @@ const styles = StyleSheet.create({
         textAlign: "center",
         color: '#da291c'
     },
-    manage: {
-        fontFamily: "Raleway",
-        fontSize: 34,
-        fontWeight: "bold",
-        fontStyle: "normal",
-        lineHeight: 38,
-        letterSpacing: 0,
-        textAlign: "center",
-        color: "#FFFFFF",
-
-    },
     footer: {
         minHeight: 63,
         maxHeight: 63,
         flex: 5
         , position: 'relative',
         bottom: -.20
+    },
+    invisibleBox: {
+        flex: 1,
+        backgroundColor: "transparent",
+        alignSelf: 'center',
+        zIndex: 1000,
+        width: "100%",
+        height: "49%",
+        position: 'absolute',
+        bottom: '0%'
     }
 });
