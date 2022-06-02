a22782d3ac121e0d47c3b250f18147794909fd97
Ahmad Farag <ahmad.k.farag@gmail.com>
2022-06-01 22:23:39 -0400

    chore: update config to use secrets

--- app.config.js
+++ app.config.js
@@ -1,6 +1,17 @@
 export default ({ config }) => {
   return {
     ...config,
-    extra: require("./config/production"),
+    extra: {
+      MEAL_FEED: process.env.MEAL_FEED,
+      ACTIVITY_FEED: process.env.ACTIVITY_FEED,
+      BHP_FEED: process.env.BHP_FEED,
+      RIVERSIDE_FEED: process.env.RIVERSIDE_FEED,
+      STAFF_FEED: process.env.STAFF_FEED,
+      STAFF_PASSWORD: process.env.STAFF_PASSWORD,
+      CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
+      CONTENTFUL_SPACE: process.env.CONTENTFUL_SPACE,
+      CONTENTFUL_ENVIRONMENT: process.env.CONTENTFUL_ENVIRONMENT,
+      //...require("./config/production"),
+    },
   };
 };
