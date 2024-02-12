affef3502fce97ffa442214de9aa36e06568ab11
Ahmad Farag <afarag@cas.org>
2024-02-12 14:07:31 +0400

    fix: updates for upgrade

--- app.config.js
+++ app.config.js
@@ -11,6 +11,7 @@ export default ({ config }) => {
       CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
       CONTENTFUL_SPACE: process.env.CONTENTFUL_SPACE,
       CONTENTFUL_ENVIRONMENT: process.env.CONTENTFUL_ENVIRONMENT,
+      // uncomment for local dev
       // ...require("./config/production"),
     },
   };
